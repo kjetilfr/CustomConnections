8d82df78cd4d014af59b7af54e670a5939d194fb
kjetilfr <kjefriborg@gmail.com>
2023-07-25 01:07:07 +0200

    You need to fill all fields to continue

--- JScripts/createScript.js
+++ JScripts/createScript.js
@@ -14,24 +14,44 @@ function run() {
 }
 
 function nextCategory(counter) {
-    count ++;
-    if (counter == 0) { //No fields are empty
-        document.getElementById("category1Content").style.display = "none";
-        document.getElementById("category2Content").style.display = "block";
+    if (counter == 0) {
+        if (document.getElementById("category1").value !== "" && document.getElementById("category1Answer1").value !== "" && document.getElementById("category1Answer2").value !== "" && document.getElementById("category1Answer3").value !== "" && document.getElementById("category1Answer4").value !== "") {
+            document.getElementById("category1Content").style.display = "none";
+            document.getElementById("category2Content").style.display = "block";
+            count ++;
+        } else {
+            alert("Please fill all fields")
+        }
     } else if (counter == 1) {
-        document.getElementById("category2Content").style.display = "none";
-        document.getElementById("category3Content").style.display = "block";
+        if (document.getElementById("category2").value !== "" && document.getElementById("category2Answer1").value !== "" && document.getElementById("category2Answer2").value !== "" && document.getElementById("category2Answer3").value !== "" && document.getElementById("category2Answer4").value !== "") {
+            document.getElementById("category2Content").style.display = "none";
+            document.getElementById("category3Content").style.display = "block";
+            count ++;
+        } else {
+            alert("Please fill all fields")
+        }
     } else if (counter == 2) {
-        document.getElementById("category3Content").style.display = "none";
-        document.getElementById("category4Content").style.display = "block";
+        if (document.getElementById("category3").value !== "" && document.getElementById("category3Answer1").value !== "" && document.getElementById("category3Answer2").value !== "" && document.getElementById("category3Answer3").value !== "" && document.getElementById("category3Answer4").value !== "") {
+            document.getElementById("category3Content").style.display = "none";
+            document.getElementById("category4Content").style.display = "block";
+            count ++;
+        } else {
+            alert("Please fill all fields")
+        }
     } else if (counter == 3) {
-        document.getElementById("category4Content").style.display = "none";
-        document.getElementById("extraDetails").style.display = "block";
-        document.getElementById("nextCategory").style.backgroundColor = "green";
-        document.getElementById("nextCategory").innerText = "Done";
+        if (document.getElementById("category4").value !== "" && document.getElementById("category4Answer1").value !== "" && document.getElementById("category4Answer2").value !== "" && document.getElementById("category4Answer3").value !== "" && document.getElementById("category4Answer4").value !== "") {
+            document.getElementById("category4Content").style.display = "none";
+            document.getElementById("extraDetails").style.display = "block";
+            document.getElementById("nextCategory").style.backgroundColor = "green";
+            document.getElementById("nextCategory").innerText = "Done";
+            count ++;
+        } else {
+            alert("Please fill all fields")
+        }
     } else {
         document.getElementById("ConnectionsBox").style.display = "none";
         document.getElementById("generateLinkContent").style.display = "block";
+        count ++;
         //document.getElementById("lives").style.display = "none";
         //showAll()
         generateShortenedLink()
